2d137fee618171631373b2f1eeb5d3970bf85edc
Rameez Arden <rameezh88@gmail.com>
2024-03-20 23:31:53 +0100

    Sync data every 20 secs

--- src/hooks/useSyncTodoData.ts
+++ src/hooks/useSyncTodoData.ts
@@ -4,6 +4,7 @@ import {syncTodosToBackend} from '../api';
 import {useDispatch, useSelector} from 'react-redux';
 import useAppState from './useAppState';
 
+// Hook that syncs the data from the Redux store with the backend.
 const useSyncTodoData = () => {
   const dispatch = useDispatch();
   const todoData = useSelector(selectTodosData);
@@ -11,12 +12,26 @@ const useSyncTodoData = () => {
   const {appInForeground} = useAppState();
 
   useEffect(() => {
-    if (appInForeground) {
-      // App in foreground. Syncing todo data to backend
+    const timer = setInterval(() => {
+      if (todoData) {
+        // Sync todo data with the backend every 20 seconds.
+        syncTodosToBackend(todoData, dispatch);
+      }
+    }, 20000);
+
+    return () => {
+      clearInterval(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [todoData]);
+
+  useEffect(() => {
+    if (appInForeground && todoData) {
+      // App in foreground. Syncing todo data with backend
       syncTodosToBackend(todoData, dispatch);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [appInForeground]);
+  }, [appInForeground, todoData]);
 };
 
 export default useSyncTodoData;
